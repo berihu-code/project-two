72cffe12a3d00e94d3c6a692bbd04af324ebe6b9
berihu-code <152714420+berihu-code@users.noreply.github.com>
2024-03-12 15:45:53 +0300

    second commit

---
+++ js/script.js
@@ -0,0 +1,90 @@
+//practice exercise
+// question 1
+// console.log(24>3)
+// console.log(2<"12")
+// console.log(0 == 2)
+
+// console.log(2.0 === 2 )
+// console.log(2.0 == "2")
+
+// console.log(2<"John")
+// console.log(2>"John")
+// console.log("2"<"2")
+// console.log("2">"12")
+// console.log("2"<"32")
+// console.log("a">"b")
+console.log(1 == 1 || 3 == 2 || 3 == 7)
+// true || false || false: The overall result is true
+
+console.log(1 == 1 && 2 == 2 && 3 == 7)
+// true && true && false: The overall result is false
+
+console.log(1 == 1 || 2 == 2 && 3 == 7)
+// 1 == 1 || (true && false): The overall result is true
+
+console.log(1 == 1 && 3 == 2 || 3 == 7)
+// false || false: The overall result is false
+console.log(1 == true && 0 > true || "31" > "9" || 10 > 5 ||!("2" == "two") || 1 == "1")
+// true && false || false || true || true
+// false || false || true || true: The overall result is true
+
+
+// quastion 2
+
+
+//  1 D
+// console.log('1' === 1) false
+//  2 B 
+// let x = (1 == true)
+// console.log(x) is true
+// 3. D 
+// let x = 10 ;
+// let y = ( x > 5 ) && ( x < 15 );
+// console.log(y) is true
+// 4. B 
+// let x = 5;
+// x+= 3   = x x + 3
+// console.log(x) is 8
+// 5. A 
+// let x = 10;
+// let y = x++;
+// console.log(y) is 10 cuz it is post - incriment
+
+
+
+
+// 6. D 
+let x = 1;
+let y = (x !== 2);
+
+// !== operator is the strict inequality operator
+
+
+
+
+// console.log(y) 
+// In this example, the !== operator is used to compare the value of x to the value 2. Since x is not equal to 2, the operator returns true.
+// 7. 4
+// the output Of (+"2" + 2)
+// console.log(+"2"+2) is 4
+// 8. 1
+// console.log(7%3) is 1
+// 9. 3
+// console.log(2+true) 3
+
+// quastion 3
+
+// let a = 1;
+// let b = 2 ;
+// c = a + b;
+// console.log(c)
+
+// quastion 3
+
+// let myFirstName;
+// let myLastName;
+// let fullName;
+// myFirstName = "Berihu";
+// myLastName = "Tesfay";
+// fullName = myFirstName + " " + myLastName;
+// console.log(fullName)
